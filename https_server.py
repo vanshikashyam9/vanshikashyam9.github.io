ab7db5fd64d7baeb260c7c6dc6afa8b6f3735c9e
Vanshika Shyam <vanshikashyam@Vanshikas-Mac-mini.local>
2026-04-01 19:20:50 -0700

    Exercise 1 done

---
+++ https_server.py
@@ -0,0 +1,12 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import ssl
+
+httpd = HTTPServer(("0.0.0.0", 8000), SimpleHTTPRequestHandler)
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain(certfile="cert.pem", keyfile="key.pem")
+
+httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+
+print("Serving HTTPS on https://localhost:8000")
+httpd.serve_forever()
